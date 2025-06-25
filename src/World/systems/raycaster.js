71301d05502fa606d9469d8d44e20932a82463e6
Milki Legesse <nitfanono@gmail.com>
2025-06-26 01:04:48 +0300

    finished with the raycastig zoom

--- src/World/systems/raycaster.js
+++ src/World/systems/raycaster.js
@@ -1,4 +1,5 @@
 import { Raycaster, Vector2, Vector3 } from "three";
+import { gsap } from "gsap";
 const mouse = new Vector2();
 const raycaster = new Raycaster();
 export function rayCaster(camera, event, scene, controls) {
@@ -10,12 +11,28 @@ export function rayCaster(camera, event, scene, controls) {
         const object = intersects[0].object;
         if (object.userData && object.userData.name) {
             console.log("Clicked on:", object);
-            const offset = new Vector3(0, 0, 300);
-            const newPos = object.position.clone().add(offset);
-            camera.position.copy(newPos);
-            camera.lookAt(object.position); // Adjust the camera position to look at the object
-            controls.target.copy(object.position);
+            const objectWorldPosition = new Vector3();
+        object.getWorldPosition(objectWorldPosition);
+
+        const direction = new Vector3()
+        .subVectors(camera.position, objectWorldPosition)
+        .normalize();
+
+        const newPos = objectWorldPosition.clone().add(direction.multiplyScalar(300)); // STEPPING AWAY TO LOOK AT THE OBJECT PROPERLY
+        //tweeny deprecated or not available so i am using gsap to animate the camera position
+        gsap.to(camera.position, {
+        x: newPos.x,
+        y: newPos.y,
+        z: newPos.z,
+        duration: 1,             
+        ease: "power2.inOut",    
+        onUpdate: () => {
+            camera.lookAt(objectWorldPosition);
+            controls.target.copy(objectWorldPosition);
             controls.update();
+  }
+});
+
             
         } else {
             console.log("Clicked on an object without a name");
